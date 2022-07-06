12fab57b26f28cf3e4573f8e0686747e9480f1b0
mjkim <thoiu610@naver.com>
2022-07-06 14:02:14 +0900

    22.07.06 last commit

---
+++ src/main/java/com/mjkim/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,4 @@
+package com.mjkim.book.springboot.domain.posts;
+
+public class Posts {
+}
--- src/main/java/com/mjkim/book/springboot/web/HelloController.java
+++ src/main/java/com/mjkim/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.mjkim.book.springboot.web;
 
+import com.mjkim.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,14 +12,20 @@ public class HelloController {
     public String hello() {
         return "hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name,amount);
+    }
 }
 
 /*
  @RestController
    - 해당 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어준다.
    - Spring에서 각 메소드마다 @ResponseBody를 선언했던것을 한번에 사용할 수 있게 해준다.
-
  @GetMapping
    - Http Method인 Get요청을 받을수 있는 API를 만들어준다.
    - 예전 Spring에서 @RequestMapping(method = RequestMethod.GET) 사용되었다.
+ @RequestParam
+   - 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
 * */
